cedba1a223e8c8ff0391b92a6e30546030aa43fe
Jonathan Yeminy <jonathanyeminy@yahoo.com>
2022-03-17 09:13:00 -0400

    commit tree 85b359b3783cac60f994cbd3235afed8cd252f93

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName(customerName){
+     customerName = customerName.toUpperCase()
+     return customerName
+}
+
+function setBestCustomer () {
+    globalThis.bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(bestCustomer){
+    globalThis.bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = "brent"
+function changeLeastFavoriteCustomer(newLeastFavorite) {
+    leastFavoriteCustomer = newLeastFavorite
+}
